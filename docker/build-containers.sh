8b85da3b723a22ac1d63cfb75bf043dfa61d178e
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-06-20 23:34:48 +0200

    add tag and push

--- docker/build-containers.sh
+++ docker/build-containers.sh
@@ -2,3 +2,9 @@
 
 docker build --file ./api/Dockerfile -t tronscan-api ../
 docker build --file ./db/Dockerfile -t tronscan-db ../
+
+docker tag tronscan-api tronscan/api
+docker tag tronscan-db tronscan/db
+
+docker push tronscan/api
+docker push tronscan/db
