4ae16a07968ed34a8fbb47ca0e04fa6ee9aa0f84
renchengchang <rencc@sina.com>
2018-08-10 15:04:49 +0800

    find all funds

--- app/org/tronscan/models/FundsModel.scala
+++ app/org/tronscan/models/FundsModel.scala
@@ -3,32 +3,29 @@ package org.tronscan.models
 import com.google.inject.{Inject, Singleton}
 import org.tronscan.db.PgProfile.api._
 import org.tronscan.db.TableRepository
-import org.joda.time.DateTime
 import play.api.db.slick.DatabaseConfigProvider
-import play.api.libs.json.Json
-import org.tronscan.App._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 
-object FundsModel {
-  implicit val format = Json.format[FundsModel]
-}
+case class FundsModel(id: Int, address: String)
 
-case class FundsModel(address: String)
-
-class FundModelTable(tag: Tag) extends Table[FundsModel](tag, "funds") {
+class FundsModelTable(tag: Tag) extends Table[FundsModel](tag, "funds") {
+  def id = column[Int]("id")
   def address = column[String]("address")
-  def * = (address) <> ((FundsModel.apply _).tupled, FundsModel.unapply)
+  def * = (id, address) <> ((FundsModel.apply _).tupled, FundsModel.unapply)
 }
 
 @Singleton()
-class FundModelRepository @Inject() (val dbConfig: DatabaseConfigProvider) extends TableRepository[FundModelTable, FundsModel] {
+class FundsModelRepository @Inject() (val dbConfig: DatabaseConfigProvider) extends TableRepository[FundsModelTable, FundsModel] {
 
-  lazy val table = TableQuery[FundModelTable]
+  lazy val table = TableQuery[FundsModelTable]
 
-  def findAll = run { implicit request =>
-    table.filter(_.address === "")
-  }
+  def findAll()(implicit executionContext: ExecutionContext) = run {
+    sql"""
+       SELECT f.address, a.balance, a.power
+       FROM funds f LEFT JOIN accounts a
+       ON f.address = a.address
+    """.as[(String, Long, Long)]
+  }.map(r => r)
 }
-
